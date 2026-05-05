b90e8c330cf2ac0467cc2f4af17c6ba6e69cf563
andrikproject <andrik.projectgas@gmail.com>
2026-05-05 12:07:28 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Ganti semua nilai di bawah ini setelah backend repository dibuat di GitHub.
-window.APP_CONFIG = {
-  GITHUB_OWNER: "andrikproject",
-  BACKEND_REPO: "eabsensi-satpam-backend",
-  BRANCH: "main",
-  DATA_PATH: "data/db.json",
-  ADMIN_PASSWORD: "admin123"
-};
